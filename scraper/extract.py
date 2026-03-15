c5ec5833009ce54182d79df55e0ac837cfccd23c
Ivan Stoliar <ivan.stoliar.data@gmail.com>
2026-03-15 20:32:44 +0200

    extract json format elaborated

--- scraper/extract.py
+++ scraper/extract.py
@@ -3,52 +3,80 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 
-load_dotenv()
 
-job_keyword = "Data Engineer"
-job_keywords = ["Data Enginner", "Data Analyst", "Backend Developer"]
-results_per_page = 5
-job_location = "Vienna"
+def extract_jobs():
 
-adzuna_id_secret = os.environ.get("ADZUNA_ID")
-adzuna_api_secret = os.environ.get("ADZUNA_API")
+    load_dotenv()
 
-base_url = 'https://api.adzuna.com/v1/api/jobs/at/search/1'
+    job_keyword = "Data Engineer"
+    job_keywords = ["Data Enginner", "Data Analyst", "Backend Developer"]
+    results_per_page = 5
+    job_location = "Vienna"
 
-payload = {
-    "app_id": adzuna_id_secret,
-    "app_key": adzuna_api_secret,
-    "results_per_page": results_per_page,
-    "what": job_keyword,
-    "where": job_location
-}
+    adzuna_id_secret = os.environ.get("ADZUNA_ID")
+    adzuna_api_secret = os.environ.get("ADZUNA_API")
 
-r = requests.get(base_url, params=payload)
-#r = requests.get('https://api.adzuna.com/v1/api/jobs/at/search/1?app_id={0}&app_key={1}&results_per_page={2}&what={3}&where={4}'.format(adzuna_id_secret, adzuna_api_secret, results_per_page,job_keyword,job_location))
+    base_url = 'https://api.adzuna.com/v1/api/jobs/at/search/1'
 
-#print(r.text)
+    payload = {
+        "app_id": adzuna_id_secret,
+        "app_key": adzuna_api_secret,
+        "results_per_page": results_per_page,
+        "what": job_keyword,
+        "where": job_location
+    }
 
-data = r.json()
+    r = requests.get(base_url, params=payload)
+    #r = requests.get('https://api.adzuna.com/v1/api/jobs/at/search/1?app_id={0}&app_key={1}&results_per_page={2}&what={3}&where={4}'.format(adzuna_id_secret, adzuna_api_secret, results_per_page,job_keyword,job_location))
 
-titles = []
-companies = []
-job_descriptions = []
-locations = []
-dates = []
+    #print(r.text)
 
+    data = r.json()
+
+   # print(data["results"][0])
 
 
-for job in data["results"]:
-    titles.append(job["title"])
-    companies.append(job["company"]["display_name"])
+    job_description = []
+    lowered_job_description = []
+    job_description.append(data["results"][0]["description"])
 
-print(titles)
-print(companies)
+    for word in job_description:
+        lowered_job_description.append(word.lower())
 
-dataset = {
-    "job_title":titles,
-    "company_name": companies
-}
+    # print(lowered_job_description)
 
-df = pd.DataFrame(dataset)
-print(df)
+    target_skills = ["python", "sql", "aws", "azure", "kafka", "java", "spark", "austria", "wien", "wir", "vollzeit", "teilzeit", "österreich"]
+    found_skills = []
+    for skill in target_skills:
+        if skill in str(lowered_job_description):
+            found_skills.append(skill)
+
+    print(lowered_job_description)
+    # print(lowered_job_description[0])
+    print(found_skills)
+
+    # titles = []
+    # companies = []
+    # job_descriptions = []
+    # locations = []
+    # dates = []
+
+
+
+    # for job in data["results"]:
+    #     titles.append(job["title"])
+    #     companies.append(job["company"]["display_name"])
+
+    # print(titles)
+    # print(companies)
+
+    # dataset = {
+    #     "job_title":titles,
+    #     "company_name": companies
+    # }
+
+    # df = pd.DataFrame(dataset)
+    # print(df)
+
+if __name__ == "__main__":
+    extract_jobs()
