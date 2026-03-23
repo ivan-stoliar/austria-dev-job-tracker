9f2b23f69f1d71430ab82d8b5ffca05d5816b0f5
Ivan Stoliar <ivan.stoliar.data@gmail.com>
2026-03-23 23:53:25 +0200

    feat(extract): implement data lake partitioning and metadata lineage wrapper, timeout in api_calls, tenacity retry logic

--- scraper/extract.py
+++ scraper/extract.py
@@ -1,11 +1,11 @@
 import requests
 from dotenv import load_dotenv
 import os
-import pandas as pd
 import json
 import logging
 from datetime import datetime
 import time
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,26 +13,57 @@ logging.basicConfig(
     )
 
 load_dotenv()
-adzuna_id_secret = os.environ.get("ADZUNA_ID")
-adzuna_api_secret = os.environ.get("ADZUNA_API")
+adzuna_id_secret = os.environ["ADZUNA_ID"]
+adzuna_api_secret = os.environ["ADZUNA_API"]
+jooble_api_secret = os.environ["JOOBLE_API"]
 
-def save_raw_json(data, source_name):
+
+@retry(
+    stop=stop_after_attempt(4),
+    wait=wait_exponential(multiplier=1, min=2, max=10),
+    retry=retry_if_exception_type(requests.exceptions.RequestException)
+)
+
+def fetch_data_with_retry(url, payload=None, is_post=False):
     """
-    Saves a JSON payload to the correct local folder based on the source name.
+    A network request function that automatically retries on failure.
+    """
+
+    if is_post:
+        response = requests.post(url, json=payload, timeout=10)
+    else:
+        response = requests.get(url, params=payload, timeout=10)
+
+    response.raise_for_status()
+
+    return response
+
+def save_raw_json(wrapped_data, source_name, country, keyword):
+    """
+    Saves a JSON payload using Data Lake partitioning:
+    data/raw/source/country/YYYY/MM/DD/keyword_jobs.json
     """
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    folder_path = os.path.abspath(os.path.join(script_dir, "..", "data", "raw", source_name))
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+
+    folder_path = os.path.abspath(os.path.join(
+        script_dir, "..", "data", "raw", source_name, country, year, month, day
+    ))
 
     os.makedirs(folder_path, exist_ok=True)
 
-    today = datetime.now().strftime("%Y-%m-%d")
-    file_path = os.path.join(folder_path, f"{today}.json")
+    safe_keyword = keyword.lower().replace(" ", "_")
+    file_path = os.path.join(folder_path, f"{safe_keyword}_jobs.json")
 
     with open(file_path, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
+        json.dump(wrapped_data, file, indent=4, ensure_ascii=False)
 
-    logging.info(f"Successfully saved {len(data)} records to {file_path}")
+    logging.info(f"Successfully saved partitioned file to: {file_path}")
+
 
 def extract_job_adzuna():
     """
@@ -41,12 +72,15 @@ def extract_job_adzuna():
 
     logging.info("Starting Adzuna extraction...")
     job_keywords = ["Data Engineer", "Data", "Backend", "Cloud", "Devops"]
+    target_country = "austria"
     MAX_PAGES = 10
 
-    all_adzuna_jobs = []
+    # all_adzuna_jobs = []
 
     for role in job_keywords:
         page = 1
+        keyword_jobs = []
+
         while page <= MAX_PAGES:
             logging.info(f"Fetching {role} - Page {page}...")
 
@@ -58,12 +92,13 @@ def extract_job_adzuna():
             }
 
             url = f'https://api.adzuna.com/v1/api/jobs/at/search/{page}'
-            r = requests.get(url, params=payload)
 
+            try:
+                r = fetch_data_with_retry(url, payload=payload, is_post=False)
 
-            if r.status_code != 200:
-                logging.error(f"API crashed on {role} page {page}. Code: {r.status_code}")
-                logging.error(f"Adzuna Error Message: {r.text}")
+            except requests.exceptions.RequestException as e:
+                logging.error(f"FATAL: Adzuna API totally failed on {role} page {page} after 4 attempts.")
+                logging.error(f"Error details: {e}")
                 break
 
             data = r.json().get('results', [])
@@ -72,25 +107,82 @@ def extract_job_adzuna():
                 logging.info(f"End of jobs for {role} reached at page {page}.")
                 break
 
-            all_adzuna_jobs.extend(data)
+            keyword_jobs.extend(data)
 
-            logging.info(f"Accumulated {len(all_adzuna_jobs)} total jobs so far...")
+            logging.info(f"Accumulated {len(keyword_jobs)} total {role} jobs so far...")
 
             page += 1
 
             time.sleep(5)
 
-    if all_adzuna_jobs:
-        save_raw_json(all_adzuna_jobs, "adzuna")
-        logging.info(f"Extraction complete! Saved {len(all_adzuna_jobs)} total jobs to today's file.")
-    else:
-        logging.warning("No jobs were found today across any keywords.")
+        if keyword_jobs:
+
+            wrapped_payload = {
+                "extracted_at": datetime.now().isoformat(),
+                "source": "adzuna",
+                "country": target_country,
+                "keyword": role,
+                "total_records": len(keyword_jobs),
+                "jobs": keyword_jobs # The raw API data goes inside this key
+            }
+
+            save_raw_json(wrapped_payload, "adzuna", target_country, role)
+            logging.info(f"Extraction complete! Saved {len(keyword_jobs)} jobs for {role} to the file.")
+        else:
+            logging.warning("No jobs were found today across any keywords.")
 
 
 
 def extract_job_jooble():
+    """
+    Extracts job data from the Jooble API.
+    """
+    logging.info("Starting Jooble extraction...")
 
-    pass
+    url = f"https://jooble.org/api/{jooble_api_secret}"
+
+    job_keywords = ["Data Engineer", "Data", "Backend", "Cloud", "Devops"]
+    MAX_PAGES = 10
+
+    all_jooble_jobs = []
+
+    for role in job_keywords:
+        page = 1
+        while page <= MAX_PAGES:
+            logging.info(f"Fetching Jooble: {role} - Page {page}...")
+
+            payload = {
+                "keywords": role,
+                "location": "Austria",
+                "page": str(page)
+            }
+
+            try:
+                r = fetch_data_with_retry(url, payload=payload, is_post=True)
+
+            except requests.exceptions.RequestException as e:
+                logging.error(f"FATAL: Jooble API totally failed on {role} page {page} after 4 attempts.")
+                logging.error(f"Error details: {e}")
+                break
+
+            data = r.json().get('jobs', [])
+
+            if not data:
+                logging.info(f"End of Jooble jobs for {role} reached at page {page}.")
+                break
+
+            all_jooble_jobs.extend(data)
+
+            page += 1
+
+            time.sleep(5)
+
+    if all_jooble_jobs:
+        save_raw_json(all_jooble_jobs, "jooble")
+        logging.info(f"Extraction complete! Saved {len(all_jooble_jobs)} Jooble jobs.")
+    else:
+        logging.warning("No jobs were found today across any keywords.")
+
 
 def extract_job_arbeitnow():
 
@@ -100,7 +192,7 @@ def extract_job_arbeitnow():
 
     logging.info("Starting Arbeitnow extraction...")
     url = "https://www.arbeitnow.com/api/job-board-api"
-    r = requests.get(url)
+    r = requests.get(url, timeout=10)
 
     if r.status_code == 200:
         data = r.json().get('data', [])
@@ -109,57 +201,11 @@ def extract_job_arbeitnow():
         logging.error(f"Arbeitnow API failed with status code: {r.status_code}")
 
 
-# def extract_jobs():
-
-    # job_description = []
-    # lowered_job_description = []
-    # job_description.append(data["results"][0]["description"])
-
-    # for word in job_description:
-    #     lowered_job_description.append(word.lower())
-
-    # print(lowered_job_description)
-
-    # target_skills = ["python", "sql", "aws", "azure", "kafka", "java", "spark", "austria", "wien", "wir", "vollzeit", "teilzeit", "österreich"]
-    # found_skills = []
-    # for skill in target_skills:
-    #     if skill in str(lowered_job_description):
-    #         found_skills.append(skill)
-
-    # print(lowered_job_description)
-    # print(lowered_job_description[0])
-    # print(found_skills)
-
-    # titles = []
-    # companies = []
-    # job_descriptions = []
-    # locations = []
-    # dates = []
-
-
-
-    # for job in data["results"]:
-    #     titles.append(job["title"])
-    #     companies.append(job["company"]["display_name"])
-
-
-    # # print(titles)
-    # # print(companies)
-
-    # dataset = {
-    #     "job_title":titles,
-    #     "company_name": companies
-    # }
-
-    # df = pd.DataFrame(dataset)
-    # print(df)
-
-
 if __name__ == "__main__":
     logging.info("Starting Daily Data Extraction Pipeline")
 
-    extract_job_arbeitnow()
+    # extract_job_arbeitnow()
     extract_job_adzuna()
-    # extract_jooble()
+    # extract_job_jooble()
 
     logging.info("Pipeline Finished")
